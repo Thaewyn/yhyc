6687cc3d167fc10fddecd07c3d3024ec49a5e55f
Thaewyn <thaewyn@gmail.com>
2021-07-30 10:13:47 -0400

    minor change, made sure we have a default root route

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.use(express.static("public"));
 
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
